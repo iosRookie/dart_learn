a8344bf7f9665fb372f416e0e1e811b750c04b00
yyg <444860018@qq.com>
2020-09-04 18:59:20 +0800

    StreamController

--- dart_stream_controller.dart
+++ dart_stream_controller.dart
@@ -1,3 +1,41 @@
+import 'dart:async';
+
 void main(List<String> args) {
-  print(args.toString());
+  var streamController = StreamController(onListen: () {
+    print("listen");
+  }, onPause: () {
+    print("pause");
+  }, onResume: () {
+    print("resume");
+  }, onCancel: () {
+    print("cancel");
+  });
+
+  var count = 0;
+  Timer.periodic(Duration(seconds: 1), (value) {
+    streamController.sink.add(count++);
+  });
+
+  print("${streamController.hasListener}");
+
+  var subscription = streamController.stream.listen((value) {
+    print("${value}");
+  }, onError: (obj, str) {
+    print("error");
+  }, onDone: () {
+    print("done");
+  }, cancelOnError: true);
+  print("${streamController.hasListener}");
+
+  Future.delayed(Duration(seconds: 5), () {
+    subscription.pause();
+  });
+
+  Future.delayed(Duration(seconds: 10), () {
+    subscription.resume();
+  });
+
+  Future.delayed(Duration(seconds: 15), () {
+    subscription.cancel();
+  });
 }
